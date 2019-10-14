a1a1fb7f37e77281768f2758bfff459143098516
Andrea-Zito-Peano-5B <56531636+Andrea-Zito-Peano-5B@users.noreply.github.com>
2019-10-14 09:05:26 +0200

    Add files via upload
    Sostituito lo scanner con uno stream

--- ClientTCP.java
+++ ClientTCP.java
@@ -5,8 +5,10 @@
  */
 package clienttcp;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.Scanner;
 import java.util.logging.Level;
@@ -23,10 +25,9 @@ public class ClientTCP {
      */
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
-        
-        Scanner in = new Scanner (System.in);
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         System.out.print(">");
-        String message ="Tassone" + in.nextLine();
+        String message ="Tassone" + in.readLine();
         
         
             Socket clientSocket = new Socket ("10.102.0.65",3333);
